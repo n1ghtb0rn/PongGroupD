2db0ee2484e26a8ab89713665c0074384d04ad01
Jo <joanne.yager@student.kyh.se>
2021-11-29 14:00:23 +0100

    First layout version

---
+++ app/src/main/java/com/kyhgroupd/ponggroupd/BreakoutActivity.kt
@@ -0,0 +1,11 @@
+package com.kyhgroupd.ponggroupd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BreakoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_breakout)
+    }
+}
