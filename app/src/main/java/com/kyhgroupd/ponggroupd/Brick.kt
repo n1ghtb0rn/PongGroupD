07f571ebe1122756d0b27c05c85a77c795d954ec
n1ghtb0rn <daniel.falkedal@gmail.com>
2021-11-29 15:21:48 +0100

    added Brick-class

--- app/src/main/java/com/kyhgroupd/ponggroupd/Brick.kt
+++ app/src/main/java/com/kyhgroupd/ponggroupd/Brick.kt
@@ -1,4 +1,22 @@
 package com.kyhgroupd.ponggroupd
 
-class Brick: GameObject() {
+import android.graphics.Canvas
+
+class Brick(startX: Int, startY: Int, color: Int): GameObject(startX, startY, color) {
+
+    val width: Int = 20
+    val height: Int = 10
+
+    fun destroy(){
+        //1. Sound effect?
+
+        //2. Animation effect? (optional)
+
+        //3. Destroy this brick! (final)
+    }
+
+    fun draw(canvas: Canvas?){
+        canvas?.drawRect(this.posX.toFloat(), this.posY.toFloat(), this.height.toFloat(), this.width.toFloat(), this.paint)
+    }
+
 }
