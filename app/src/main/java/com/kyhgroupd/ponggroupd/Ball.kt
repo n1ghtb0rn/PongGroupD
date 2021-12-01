be1131cfc64f65c8486ba1a42712e7755a27127d
n1ghtb0rn <daniel.falkedal@gmail.com>
2021-12-01 14:39:15 +0100

    added ball radius scaling + wall collision

--- app/src/main/java/com/kyhgroupd/ponggroupd/Ball.kt
+++ app/src/main/java/com/kyhgroupd/ponggroupd/Ball.kt
@@ -5,9 +5,13 @@ import android.graphics.Paint
 
 class Ball(startX: Int, startY: Int, color: Int) : GameObject(startX, startY, color) {
 
-    val radius : Int = 20
-    var speedX: Int = 5
-    var speedY: Int = 5
+    var radius : Int = 0
+    var speedX: Int = -5
+    var speedY: Int = -5
+
+    init {
+        radius = DataManager.screenSizeX / 50
+    }
 
     override fun draw(canvas: Canvas?) {
         canvas?.drawCircle(this.posX.toFloat(), this.posY.toFloat(), this.radius.toFloat(), this.paint)
@@ -16,5 +20,19 @@ class Ball(startX: Int, startY: Int, color: Int) : GameObject(startX, startY, co
     override fun update(){
         posX += speedX
         posY += speedY
+
+        //Screen boarder check
+        if(this.posY < 0+this.radius){
+            this.speedY = 5
+        }
+        if(this.posY > DataManager.screenSizeY-this.radius){
+            this.speedY = -5
+        }
+        if(this.posX < 0+this.radius){
+            this.speedX = 5
+        }
+        if(this.posX > DataManager.screenSizeX-this.radius){
+            this.speedX = -5
+        }
     }
 }
