8864c9cf43f6dab934efb1b8d3ced1e71601cb5b
Jo <joanne.yager@student.kyh.se>
2021-12-01 14:14:31 +0100

    Skärmstorlek anpassning

--- app/src/main/java/com/kyhgroupd/ponggroupd/GameView.kt
+++ app/src/main/java/com/kyhgroupd/ponggroupd/GameView.kt
@@ -3,9 +3,11 @@ package com.kyhgroupd.ponggroupd
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.util.DisplayMetrics
 import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import androidx.appcompat.app.AppCompatActivity
 
 class GameView(context: Context): SurfaceView(context), SurfaceHolder.Callback, Runnable {
 
@@ -14,8 +16,14 @@ class GameView(context: Context): SurfaceView(context), SurfaceHolder.Callback, 
     lateinit var canvas: Canvas
     var mHolder: SurfaceHolder? = holder
 
+
     init {
         mHolder?.addCallback(this)
+
+        var activity: AppCompatActivity = context as AppCompatActivity
+        var displayMetrics: DisplayMetrics = activity.resources.displayMetrics
+        DataManager.screenSizeX = displayMetrics.widthPixels
+
         setup()
     }
 
