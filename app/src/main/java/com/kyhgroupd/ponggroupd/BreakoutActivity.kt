116c587c7e5ae9f4b58e3949d5b3ba97ff792632
Jo <joanne.yager@student.kyh.se>
2021-12-01 10:55:07 +0100

    GameView class

--- app/src/main/java/com/kyhgroupd/ponggroupd/BreakoutActivity.kt
+++ app/src/main/java/com/kyhgroupd/ponggroupd/BreakoutActivity.kt
@@ -1,80 +1,13 @@
 package com.kyhgroupd.ponggroupd
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import android.view.View
-import com.kyhgroupd.ponggroupd.databinding.ActivityBreakoutBinding
 
-class BreakoutActivity : AppCompatActivity(), SurfaceHolder.Callback, View.OnTouchListener {
-
-    private lateinit var binder : ActivityBreakoutBinding
+class BreakoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binder = ActivityBreakoutBinding.inflate(layoutInflater)
-        setContentView(binder.root)
+        setContentView(GameView(this))
 
-        binder.surfaceView.holder.addCallback(this)
-
-        binder.surfaceView.setOnTouchListener(this)
-        init()
-
-    }
-
-    override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
-        if (view !is SurfaceView) {
-            return false
-        }
-        draw()
-        return true
-    }
-
-    override fun surfaceCreated(p0: SurfaceHolder) {
-
-    }
-
-    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
-
-    }
-
-    override fun surfaceDestroyed(p0: SurfaceHolder) {
-
-    }
-
-    private fun init() {
-        var ball = Ball(475, 1000, Color.WHITE)
-        var paddle : Paddle = Paddle(500,1600, Color.WHITE)
-        var brick1 : Brick = Brick(100, 300, Color.WHITE)
-        var brick2 : Brick = Brick(200, 300, Color.WHITE)
-        var brick3 : Brick = Brick(300, 300, Color.WHITE)
-        var brick4 : Brick = Brick(400, 300, Color.WHITE)
-        GameManager.gameObjects.add(ball)
-        GameManager.gameObjects.add(paddle)
-        GameManager.gameObjects.add(brick1)
-        GameManager.gameObjects.add(brick2)
-        GameManager.gameObjects.add(brick3)
-        GameManager.gameObjects.add(brick4)
-    }
-
-    private fun draw(){
-        val canvas: Canvas? = binder.surfaceView.holder.lockCanvas()
-        val surfaceBackground = Paint()
-        surfaceBackground.color = Color.BLACK
-
-        canvas?.drawRect(0f, 0f, binder.surfaceView.width.toFloat(), binder.surfaceView.height.toFloat(), surfaceBackground)
-
-        for (gameObject in GameManager.gameObjects) {
-            gameObject.draw(canvas)
-            println("$gameObject.posX / $gameObject.posY")
-        }
-
-        binder.surfaceView.holder.unlockCanvasAndPost(canvas)
-        binder.surfaceView.setZOrderOnTop(true)
     }
 }
---
+++ app/src/main/java/com/kyhgroupd/ponggroupd/GameView.kt
@@ -0,0 +1,94 @@
+package com.kyhgroupd.ponggroupd
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class GameView(context: Context): SurfaceView(context), SurfaceHolder.Callback, Runnable {
+
+    private var thread: Thread? = null
+    private var running = false
+    lateinit var canvas: Canvas
+    var mHolder: SurfaceHolder? = holder
+
+    init {
+        mHolder?.addCallback(this)
+        setup()
+    }
+
+    private fun setup() {
+        var ball = Ball(475, 1000, Color.WHITE)
+        var paddle : Paddle = Paddle(500,1600, Color.WHITE)
+        var brick1 : Brick = Brick(100, 300, Color.WHITE)
+        var brick2 : Brick = Brick(200, 300, Color.WHITE)
+        var brick3 : Brick = Brick(300, 300, Color.WHITE)
+        var brick4 : Brick = Brick(400, 300, Color.WHITE)
+        DataManager.paddle = paddle
+        DataManager.gameObjects.add(ball)
+        DataManager.gameObjects.add(paddle)
+        DataManager.gameObjects.add(brick1)
+        DataManager.gameObjects.add(brick2)
+        DataManager.gameObjects.add(brick3)
+        DataManager.gameObjects.add(brick4)
+    }
+
+    fun start() {
+        running = true
+        thread = Thread(this)
+        thread?.start()
+    }
+
+    fun stop() {
+        running = false
+        try {
+            thread?.join()
+        } catch (e: InterruptedException){
+            e.printStackTrace()
+        }
+    }
+
+    fun update(){
+        for (gameObject in DataManager.gameObjects) {
+            gameObject.update()
+        }
+    }
+
+    private fun draw(){
+        canvas = mHolder!!.lockCanvas()
+        canvas.drawColor(Color.BLACK)
+
+        for (gameObject in DataManager.gameObjects) {
+            gameObject.draw(canvas)
+        }
+
+        mHolder!!.unlockCanvasAndPost(canvas)
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder) {
+        start()
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+        stop()
+    }
+
+    override fun run() {
+        while (running){
+            update()
+            draw()
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if(event != null){
+            DataManager.paddle!!.posX = event.x.toInt()
+        }
+        return true
+    }
+}
