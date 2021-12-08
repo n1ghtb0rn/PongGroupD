2d208732c86837fa6590bf19acef22e862499d65
Jo <joanne.yager@student.kyh.se>
2021-12-08 13:58:59 +0100

    Game Over intents

--- app/src/main/java/com/kyhgroupd/ponggroupd/GameOverActivity.kt
+++ app/src/main/java/com/kyhgroupd/ponggroupd/GameOverActivity.kt
@@ -1,11 +1,27 @@
 package com.kyhgroupd.ponggroupd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kyhgroupd.ponggroupd.databinding.ActivityGameOverBinding
 
 class GameOverActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGameOverBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_over)
+        binding = ActivityGameOverBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMainMenu.setOnClickListener{
+            Intent(this, MainActivity :: class.java)
+                .apply { startActivity(this) }
+        }
+
+        binding.btnPlayAgain.setOnClickListener{
+            Intent(this, BreakoutActivity :: class.java)
+                .apply { startActivity(this) }
+        }
     }
 }
