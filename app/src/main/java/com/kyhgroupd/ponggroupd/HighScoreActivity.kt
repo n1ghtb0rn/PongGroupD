f75fea2217c9e7632dd935fac441517c1efc1ba6
n1ghtb0rn <daniel.falkedal@gmail.com>
2021-12-09 14:48:40 +0100

    add high score activity

---
+++ app/src/main/java/com/kyhgroupd/ponggroupd/HighScoreActivity.kt
@@ -0,0 +1,11 @@
+package com.kyhgroupd.ponggroupd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HighScoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_high_score)
+    }
+}
