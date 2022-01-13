6279caae8de8d1a28a5bd43147ef78b606ec3ead
jojojokyh <joanne.yager@student.kyh.se>
2022-01-13 09:50:50 +0100

    Make pong mode chooser buttons grey when pressed

--- app/src/main/java/com/kyhgroupd/ponggroupd/activitys/PongModeChooserActivity.kt
+++ app/src/main/java/com/kyhgroupd/ponggroupd/activitys/PongModeChooserActivity.kt
@@ -1,7 +1,9 @@
 package com.kyhgroupd.ponggroupd.activitys
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kyhgroupd.ponggroupd.GameManager
@@ -18,31 +20,69 @@ class PongModeChooserActivity : AppCompatActivity() {
 
         GameManager.pongPlayerMode = 1
         GameManager.pongDifficultyLevel = "medium"
+        buttonColorChange()
 
         binder.btnSinglePlayer.setOnClickListener {
             GameManager.pongPlayerMode = 1
+            buttonColorChange()
         }
 
         binder.btnTwoPlayer.setOnClickListener {
             GameManager.pongPlayerMode = 2
+            buttonColorChange()
         }
 
         binder.btnEasy.setOnClickListener {
             GameManager.pongDifficultyLevel = "easy"
+            buttonColorChange()
         }
 
         binder.btnMedium.setOnClickListener {
             GameManager.pongDifficultyLevel = "medium"
+            buttonColorChange()
         }
 
         binder.btnHard.setOnClickListener {
             GameManager.pongDifficultyLevel = "hard"
+            buttonColorChange()
         }
 
         binder.btnStartGame.setOnClickListener {
             val intent = Intent(this, GameActivity::class.java)
             GameManager.shouldReset = true
             startActivity(intent)
+        }
+    }
+
+    fun buttonColorChange() {
+        val pressedColor = Color.LTGRAY
+        val unpressedColor = Color.WHITE
+        when (GameManager.pongPlayerMode) {
+            1 -> {
+                binder.btnSinglePlayer.setBackgroundColor(pressedColor)
+                binder.btnTwoPlayer.setBackgroundColor(unpressedColor)
+            }
+            2 -> {
+                binder.btnSinglePlayer.setBackgroundColor(unpressedColor)
+                binder.btnTwoPlayer.setBackgroundColor(pressedColor)
+            }
+        }
+        when (GameManager.pongDifficultyLevel) {
+            "easy" -> {
+                binder.btnEasy.setBackgroundColor(pressedColor)
+                binder.btnMedium.setBackgroundColor(unpressedColor)
+                binder.btnHard.setBackgroundColor(unpressedColor)
+            }
+            "medium" -> {
+                binder.btnEasy.setBackgroundColor(unpressedColor)
+                binder.btnMedium.setBackgroundColor(pressedColor)
+                binder.btnHard.setBackgroundColor(unpressedColor)
+            }
+            "hard" -> {
+                binder.btnEasy.setBackgroundColor(unpressedColor)
+                binder.btnMedium.setBackgroundColor(unpressedColor)
+                binder.btnHard.setBackgroundColor(pressedColor)
+            }
         }
     }
 }
