91a63d4db768fdb4fb03f87fc80b84606320abe2
joakim2kyh <90765156+joakim2kyh@users.noreply.github.com>
2022-01-14 14:54:55 +0100

    Comments GameText

--- app/src/main/java/com/kyhgroupd/ponggroupd/gameobjects/GameText.kt
+++ app/src/main/java/com/kyhgroupd/ponggroupd/gameobjects/GameText.kt
@@ -11,7 +11,7 @@ import com.kyhgroupd.ponggroupd.R
 import com.kyhgroupd.ponggroupd.UIManager
 
 /**
- *
+ * Inherits from GameObject. Used for writing text on canvas.
  *
  * @param startX X position
  * @param startY Y position
@@ -19,37 +19,31 @@ import com.kyhgroupd.ponggroupd.UIManager
  */
 @RequiresApi(Build.VERSION_CODES.O)
 open class GameText(startX: Int, startY: Int, color: Int) : GameObject(startX, startY, color) {
+
     var textString: String = ""
 
     init {
         this.textString = ""
 
         this.paint.textSize = UIManager.textSize
-        this.paint.typeface = ResourcesCompat.getFont(
-            GameManager.context as AppCompatActivity,
-            R.font.aldrich
-        )
+        this.paint.typeface = ResourcesCompat.getFont(GameManager.context as AppCompatActivity, R.font.aldrich)
 
         this.grayPaint.color = Color.WHITE
         this.grayPaint.textSize = UIManager.textSize
-        this.grayPaint.typeface = ResourcesCompat.getFont(
-            GameManager.context as AppCompatActivity,
-            R.font.aldrich
-        )
+        this.grayPaint.typeface = ResourcesCompat.getFont(GameManager.context as AppCompatActivity, R.font.aldrich)
     }
 
     override fun draw(canvas: Canvas?) {
+        // Colors
         if(GameManager.useColors){
             canvas?.drawText(textString, startX.toFloat(), startY.toFloat(), this.paint)
         }
+        // Greyscale
         else{
             canvas?.drawText(textString, startX.toFloat(), startY.toFloat(), this.grayPaint)
         }
-
     }
 
     override fun update() {
-
     }
-
 }
