e0660e080a33e2a522f5da4c1365445eab7035de
joakim2kyh <90765156+joakim2kyh@users.noreply.github.com>
2022-01-17 09:15:03 +0100

    Comments PlayerScore.kt

--- app/src/main/java/com/kyhgroupd/ponggroupd/PlayerScore.kt
+++ app/src/main/java/com/kyhgroupd/ponggroupd/PlayerScore.kt
@@ -1,3 +1,9 @@
 package com.kyhgroupd.ponggroupd
 
+/**
+ * Class for storing username and score.
+ *
+ * @param username String
+ * @param score Int
+ */
 class PlayerScore(val username: String, val score: Int)
